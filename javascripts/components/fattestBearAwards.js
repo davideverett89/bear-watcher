1f6bc17a8f3e36748c12d47bbd7e5fdb142105f4
David Everett <davideverett1989@gmail.com>
2020-03-03 20:31:29 -0600

    Readme finish.  Ready to turn in.

--- javascripts/components/fattestBearAwards.js
+++ javascripts/components/fattestBearAwards.js
@@ -8,18 +8,19 @@ const showfirstPlace = () => {
     let firstPlaceWinner = bearData.sortPercentages()[0];
     const emptyCheck = bearData.getBears().some((bear) => bear.attemptedCatches.length === 0);
     if (firstPlaceWinner === undefined) {
-        alert("No first place winner has been determined.");
+        alert("Not enough contestants.");
     } else if (emptyCheck){
-        alert("Has not started yet.");
+        alert("Results pending.");
+    } else {
+        domString +=    '<div>';
+        domString +=        `<h2 class="display-4">First Place Winner: ${firstPlaceWinner.name}!</h2>`;
+        domString +=        `<img class="mb-3" src="${firstPlaceWinner.image}" alt="${firstPlaceWinner.name}">`;
+        domString +=        `<p>Catch Percentage: ${firstPlaceWinner.catchPercentage}%</p>`;
+        domString +=    '</div>';
+        $('#singlebearModal').modal('show');
+        utilities.printToDom("single-view", domString);
+        document.getElementById("close-single-view").addEventListener("click", singleView.closeSingleEvent);
     }
-    domString +=    '<div>';
-    domString +=        `<h2 class="display-4">First Place Winner: ${firstPlaceWinner.name}!</h2>`;
-    domString +=        `<img class="mb-3" src="${firstPlaceWinner.image}" alt="${firstPlaceWinner.name}">`;
-    domString +=        `<p>Catch Percentage: ${firstPlaceWinner.catchPercentage}%</p>`;
-    domString +=    '</div>';
-    $('#singlebearModal').modal('show');
-    utilities.printToDom("single-view", domString);
-    document.getElementById("close-single-view").addEventListener("click", singleView.closeSingleEvent);
 }
 
 const showSecondPlace = () => {
@@ -27,32 +28,39 @@ const showSecondPlace = () => {
     let secondPlaceWinner = bearData.sortPercentages()[1];
     const emptyCheck = bearData.getBears().some((bear) => bear.attemptedCatches.length === 0);
     if (secondPlaceWinner === undefined) {
-        alert("No second place winner has been determined.");
-    } else if ()
-    domString +=    '<div>';
-    domString +=        `<h2 class="display-4">Second Place Winner: ${secondPlaceWinner.name}!</h2>`;
-    domString +=        `<img class="mb-3" src="${secondPlaceWinner.image}" alt="${secondPlaceWinner.name}">`;
-    domString +=        `<p>Catch Percentage: ${secondPlaceWinner.catchPercentage}%</p>`;
-    domString +=    '</div>';
-    $('#singlebearModal').modal('show');
-    utilities.printToDom("single-view", domString);
-    document.getElementById("close-single-view").addEventListener("click", singleView.closeSingleEvent);
+        alert("Not enough contestants.");
+    } else if (emptyCheck) {
+        alert("Results pending.");
+    } else {
+        domString +=    '<div>';
+        domString +=        `<h2 class="display-4">Second Place Winner: ${secondPlaceWinner.name}!</h2>`;
+        domString +=        `<img class="mb-3" src="${secondPlaceWinner.image}" alt="${secondPlaceWinner.name}">`;
+        domString +=        `<p>Catch Percentage: ${secondPlaceWinner.catchPercentage}%</p>`;
+        domString +=    '</div>';
+        $('#singlebearModal').modal('show');
+        utilities.printToDom("single-view", domString);
+        document.getElementById("close-single-view").addEventListener("click", singleView.closeSingleEvent);
+    }
 }
 
 const showThirdPlace = () => {
     let domString = "";
     let thirdPlaceWinner = bearData.sortPercentages()[2];
+    const emptyCheck = bearData.getBears().some((bear) => bear.attemptedCatches.length === 0);
     if (thirdPlaceWinner === undefined) {
-        alert("No third place winner has been determined.");
+        alert("Not enough contestants.");
+    } else if (emptyCheck) {
+        alert("Result pending.");
+    } else {
+        domString +=    '<div>';
+        domString +=        `<h2 class="display-4">Third Place Winner: ${thirdPlaceWinner.name}!</h2>`;
+        domString +=        `<img class="mb-3" src="${thirdPlaceWinner.image}" alt="${thirdPlaceWinner.name}">`;
+        domString +=        `<p>Catch Percentage: ${thirdPlaceWinner.catchPercentage}%</p>`;
+        domString +=    '</div>';
+        $('#singlebearModal').modal('show');
+        utilities.printToDom("single-view", domString);
+        document.getElementById("close-single-view").addEventListener("click", singleView.closeSingleEvent);
     }
-    domString +=    '<div>';
-    domString +=        `<h2 class="display-4">Third Place Winner: ${thirdPlaceWinner.name}!</h2>`;
-    domString +=        `<img class="mb-3" src="${thirdPlaceWinner.image}" alt="${thirdPlaceWinner.name}">`;
-    domString +=        `<p>Catch Percentage: ${thirdPlaceWinner.catchPercentage}%</p>`;
-    domString +=    '</div>';
-    $('#singlebearModal').modal('show');
-    utilities.printToDom("single-view", domString);
-    document.getElementById("close-single-view").addEventListener("click", singleView.closeSingleEvent);
 }
 
 
